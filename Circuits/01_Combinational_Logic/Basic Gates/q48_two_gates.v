dea94ca989220f4eaa49d3c882ecd831c158e0e2
Ojas Vaidya <ojasvaidya17@gmail.com>
2026-06-04 23:08:46 +0530

    feat: implement cascaded XNOR and XOR two-gate circuit

---
+++ Circuits/01_Combinational_Logic/Basic Gates/q48_two_gates.v
@@ -0,0 +1,13 @@
+module top_module (
+    input in1,
+    input in2,
+    input in3,
+    output out);
+ 
+    // declare the intermediate wire
+    wire out1;
+    
+    assign out1 = ~(in1 ^ in2);
+    assign out = out1 ^ in3;
+
+endmodule
