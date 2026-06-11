ee0614b28b2a3e5809b282012185d6cf167f1e15
Ojas Vaidya <ojasvaidya17@gmail.com>
2026-06-11 23:04:13 +0530

    implement K-map reduction using vector inputs and Don't Cares

---
+++ Circuits/01_Combinational_Logic/04_Karnaugh_Maps_to_Circuits/q77_min_sop_pos.v
@@ -0,0 +1,23 @@
+/*
+A single-output digital system with four inputs (a,b,c,d) generates a logic-1 when 2, 7, or 15 appears on the inputs, and a logic-0 when 0, 1, 4, 5, 6, 9, 10, 13, or 14 appears. The input conditions for the numbers 3, 8, 11, and 12 never occur in this system. For example, 7 corresponds to a,b,c,d being set to 0,1,1,1, respectively.
+
+Determine the output out_sop in minimum SOP form, and the output out_pos in minimum POS form.
+
+*/
+
+module top_module (
+    input wire a,
+    input wire b,
+    input wire c,
+    input wire d,
+    output wire out_sop,
+    output wire out_pos
+); 
+
+    // Sum of Products (Grouping 1s)
+    assign out_sop = (c & d) | (~a & ~b & c);
+    
+    // Product of Sums (Grouping 0s, then inverting)
+    assign out_pos = c & (~b | d) & (~a | d);
+
+endmodule
