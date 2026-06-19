885855d1d4c0dff1241888dea3d3725dd167cb17
Ojas Vaidya <ojasvaidya17@gmail.com>
2026-06-19 23:05:02 +0530

    implement 4-bit decade counter with synchronous reset

---
+++ Circuits/02_Sequential_Logic/02_Counters/q100_decade_counter.v
@@ -0,0 +1,17 @@
+module top_module (
+    input wire clk,
+    input wire reset,        // Synchronous active-high reset
+    output reg [3:0] q       
+);
+
+    always @(posedge clk) begin
+        if (reset) begin
+            q <= 4'b0000;    
+        end else if (q == 4'd9) begin
+            q <= 4'b0000;    
+        end else begin
+            q <= q + 1'b1;   
+        end
+    end
+
+endmodule
