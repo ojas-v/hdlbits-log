a1988caf43e0e8ee2425193f3ca6c9816773a9e1
ojas-v <ojasvaidya17@gmail.com>
2026-06-24 22:22:46 +0530

    implement 3-bit LFSR mapped to DE1-SoC board switches and LEDs

---
+++ Circuits/02_Sequential_Logic/03_Shift_Registers/q111_3bit_LFSR.v
@@ -0,0 +1,17 @@
+module top_module (
+	input [2:0] SW,      // R
+	input [1:0] KEY,     // L and clk
+	output [2:0] LEDR);  // Q
+
+    always@(posedge KEY[0]) begin
+        if(KEY[1]) begin
+        	LEDR <= SW; // synchronous load
+        end
+        else begin
+        	LEDR[0] <= LEDR[2];
+            LEDR[1] <= LEDR[0];
+            LEDR[2] <= LEDR[1] ^ LEDR[2];
+        end
+    end
+
+endmodule
