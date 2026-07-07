ebe7909258f813283ca7e2463dbfd9bc2a82ec33
Ojas Vaidya <ojasvaidya17@gmail.com>
2026-07-07 23:55:39 +0530

    implement serial 2s complementer FSM using a 2-state Mealy architecture with one-hot encoding

--- Circuits/02_Sequential_Logic/05_Finite_State_Machines/q141_2s_complementor_moore.v
+++ Circuits/02_Sequential_Logic/05_Finite_State_Machines/q141_2s_complementor_moore.v
@@ -1,5 +1,3 @@
-`default_nettype none
-
 module top_module (
     input clk,
     input areset,
@@ -13,13 +11,13 @@ module top_module (
     
     reg [1:0] state, next_state;
     
-    // 1. Next-State Logic (One-Hot Boolean Routing)
+    // Next-State Logic (One-Hot Boolean Routing)
     always @(*) begin
         next_state[0] = state[0] & ~x;
         next_state[1] = (state[0] & x) | state[1];
     end
     
-    // 2. State Memory (Asynchronous Reset)
+    // State Memory (Asynchronous Reset)
     always @(posedge clk or posedge areset) begin
         if (areset) begin
             state <= A;
@@ -28,7 +26,7 @@ module top_module (
         end
     end
     
-    // 3. Mealy Output Logic
+    // Mealy Output Logic
     // Z is 1 if (we are waiting for first 1 AND receive a 1) 
     // OR (we are inverting AND receive a 0)
     assign z = (state == A && x == 1'b1) || (state == B && x == 1'b0);
