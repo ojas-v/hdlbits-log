cd5db6310f4b893bbbd575a814ad6f6446ae8608
Ojas Vaidya <ojasvaidya17@gmail.com>
2026-07-14 22:43:10 +0530

    resolve instantiation collisions and select logic in 4-to-1 mux

---
+++ Verification_Reading_Simulations/01_Finding_Bugs_In_Code/q161_mux4.v
@@ -0,0 +1,12 @@
+/*
+This 4-to-1 multiplexer doesn't work. Fix the bug(s).
+
+You are provided with a bug-free 2-to-1 multiplexer:
+
+module mux2 (
+    input sel,
+    input [7:0] a,
+    input [7:0] b,
+    output [7:0] out
+);
+*/
