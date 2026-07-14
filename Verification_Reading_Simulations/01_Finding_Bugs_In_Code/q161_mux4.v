e4727f08d241223d7e1e7ec536cf079edf1f0195
Ojas Vaidya <ojasvaidya17@gmail.com>
2026-07-14 22:48:46 +0530

    resolve bitwise vs logical NOT error in zero flag logic

--- Verification_Reading_Simulations/01_Finding_Bugs_In_Code/q161_mux4.v
+++ Verification_Reading_Simulations/01_Finding_Bugs_In_Code/q161_mux4.v
@@ -10,3 +10,39 @@ module mux2 (
     output [7:0] out
 );
 */
+
+module top_module (
+    input [1:0] sel,
+    input [7:0] a,
+    input [7:0] b,
+    input [7:0] c,
+    input [7:0] d,
+    output [7:0] out  
+); 
+    
+    wire [7:0] out0, out1;
+    
+    // First stage muxes driven by sel[0]
+    mux2 u0 ( 
+        .sel(sel[0]), 
+        .a(a), 
+        .b(b), 
+        .out(out0) 
+    );
+    
+    mux2 u1 ( 
+        .sel(sel[0]), 
+        .a(c), 
+        .b(d), 
+        .out(out1) 
+    );
+    
+    // Second stage mux driven by sel[1]
+    mux2 u2 ( 
+        .sel(sel[1]), 
+        .a(out0), 
+        .b(out1), 
+        .out(out) 
+    );
+
+endmodule
---
+++ Verification_Reading_Simulations/01_Finding_Bugs_In_Code/q162_addsub.v
@@ -0,0 +1,28 @@
+/*
+The following adder-subtractor with zero flag doesn't work. Fix the bug(s).
+*/
+// synthesis verilog_input_version verilog_2001
+module top_module ( 
+    input do_sub,
+    input [7:0] a,
+    input [7:0] b,
+    output reg [7:0] out,
+    output reg result_is_zero
+);
+
+    always @(*) begin
+        // Perform addition or subtraction
+        case (do_sub)
+          1'b0: out = a + b;
+          1'b1: out = a - b;
+        endcase
+
+        if (out == 8'b0) begin
+            result_is_zero = 1'b1;
+        end
+        else begin
+            result_is_zero = 1'b0;
+        end
+    end
+
+endmodule
