ec951327de6df09d8fb13ea9e67bea4dd999d15b
Ojas Vaidya <ojasvaidya17@gmail.com>
2026-07-14 21:06:21 +0530

    fix: resolve width mismatch and inverted selection in bugs_mux2

--- Verification_Reading_Simulations/01_Finding_Bugs_In_Code/q158_mux.v
+++ Verification_Reading_Simulations/01_Finding_Bugs_In_Code/q158_mux.v
@@ -1,2 +1,13 @@
 /* remove bugs
 */
+module top_module (
+    input sel,
+    input [7:0] a,
+    input [7:0] b,
+    output [7:0] out  
+);
+
+    // sel = 1 outputs 'a', sel = 0 outputs 'b'
+    assign out = sel ? a : b;
+
+endmodule
