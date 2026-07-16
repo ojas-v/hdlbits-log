4ee958a44c86f7518fb7b90869fd7910f31314c4
ojas-v <ojasvaidya17@gmail.com>
2026-07-16 21:27:01 +0530

    feat: solve q168 combinational circuit 5 (4-bit multiplexer)

---
+++ Verification_Reading_Simulations/02_Build_Circuits/q168_comb_ckt5.v
@@ -0,0 +1,21 @@
+module top_module (
+    input [3:0] a,
+    input [3:0] b,
+    input [3:0] c,
+    input [3:0] d,
+    input [3:0] e,
+    output reg [3:0] q 
+);
+
+    // waveform behaves like a mux with c as the selector
+    always @(*) begin
+        case (c)
+            4'd0: q = b;
+            4'd1: q = e;
+            4'd2: q = a;
+            4'd3: q = d;
+            default: q = 4'hf; // 4'hf is hex for 1111
+        endcase
+    end
+
+endmodule
